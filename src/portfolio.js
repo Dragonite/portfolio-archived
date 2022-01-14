b535a399f48c2ec7141f50a2071f3087aadfa5e3
Haolin <haolin97@gmail.com>
2022-01-14 15:33:46 +0800

    Update url for portfolio

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://dragonite.github.io/',
+  homepage: 'https://dragonite.github.io/portfolio/',
   title: "Haolin Wu's Portfolio",
 }
 
